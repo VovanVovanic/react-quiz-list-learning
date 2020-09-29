d2087aa7e9775abd77120acce32e48c7cb8aaa80
Vladimir <vladimirplotnikov371@gmail.com>
2020-09-29 20:49:01 +0300

    auth inputs creation

--- src/containers/auth/auth.js
+++ src/containers/auth/auth.js
@@ -1,14 +1,88 @@
 import React, { Component } from 'react'
+import Button from '../../ui/buttons'
+import Input from '../../ui/input'
 import classes from './auth.module.css'
 
 class Auth extends Component {
-    render() {
-        return (
-            <div className={classes.Auth}>
-                <h2>Auth</h2>
-            </div>
-        )
+  state = {
+    formControls: {
+      email: {
+        type: "email",
+        label: "Email",
+        placeholder: "Enter your email here",
+        errorMsg: "Wrong email format",
+        valid: false,
+        touched: false,
+        validate: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        type: "password",
+        label: "Password",
+        placeholder: "Enter your password here",
+        errorMsg: "Too short password",
+        valid: false,
+        touched: false,
+        validate: {
+          required: true,
+          minLength: 6
+        },
+      },
+    },
+  };
+  onLogin = () => {
+    console.log("ok");
+  };
+  onRegister = () => {
+    console.log("okok");
+    };
+    onInputChange = (e, control) => {
+        console.log(e.target.value, control)
     }
+    onInputsRender = () => {
+        return Object.keys(this.state.formControls).map((control, i) => {
+            const currentControl = this.state.formControls[control]
+            const {value, type, label, placeholder, errorMsg, valid, touched, validate} = currentControl
+            return (
+                <Input
+                    key={i}
+                    value={value}
+                    type={type}
+                    label={label}
+                    placeholder={placeholder}
+                    errorMsg={errorMsg}
+                    valid={valid}
+                    touched={touched}
+                    shouldValidate={!!validate}
+                    onChange={(e)=>this.onInputChange(e, control)}
+                
+                
+                />
+            )
+        })
+    }
+  render() {
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h2>Authorization</h2>
+          <form
+            className={classes.AuthForm}
+            onSubmit={(e) => e.preventDefault()}>
+            {this.onInputsRender()}
+            <Button onClick={this.onLogin} type="success">
+              Log In
+            </Button>
+            <Button onClick={this.onRegister} type="primary">
+              Register
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Auth
---
+++ src/ui/input/input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classes from './input.module.css'
+
+const Input = ({label, errorMsg, type, valid, touched, shouldValidate, onChange, placeholder }) => {
+    const cls = [
+        classes.Input
+    ]
+    const isValid = (valid, touched, shouldValidate) =>{
+        return !valid && touched && shouldValidate
+    }
+    if ((isValid(valid, touched, shouldValidate))) {
+      cls.push(classes.invalid);
+    }
+    const htmlFor = `${type}${Math.random().toFixed(3)}`
+    return (
+      <div className={cls.join(" ")}>
+        <label htmlFor={htmlFor}>{label}</label>
+        <input
+          id={htmlFor}
+          type={type}
+          placeholder={placeholder}
+          valid={valid}
+          touched={touched}
+          onChange={onChange}
+          shouldValidate={shouldValidate}
+        />
+        {isValid(valid, touched, shouldValidate) ? <span>{errorMsg}</span> : null}
+        
+      </div>
+    );
+}
+
+export default Input
