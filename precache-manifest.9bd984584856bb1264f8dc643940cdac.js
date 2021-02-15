3d6e7b0709285adad1855b94ee47dc46102fe4e1
Vladimir <vladimirplotnikov371@gmail.com>
2021-02-15 23:56:48 +0200

    Updates

---
+++ precache-manifest.9bd984584856bb1264f8dc643940cdac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a921004e5c29c2c96d3424a5d696804",
+    "url": "/react-quiz-list-learning/index.html"
+  },
+  {
+    "revision": "04bff65a6334cc6c4a04",
+    "url": "/react-quiz-list-learning/static/css/main.7a6811f2.chunk.css"
+  },
+  {
+    "revision": "9e587f4e76735e2a9d99",
+    "url": "/react-quiz-list-learning/static/js/2.c5221285.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/react-quiz-list-learning/static/js/2.c5221285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bff65a6334cc6c4a04",
+    "url": "/react-quiz-list-learning/static/js/main.78a2e9af.chunk.js"
+  },
+  {
+    "revision": "f5e498d396765ea17a33",
+    "url": "/react-quiz-list-learning/static/js/runtime-main.90a7b954.js"
+  }
+]);
